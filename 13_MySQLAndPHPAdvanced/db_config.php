1480ca48ab54437e7c286584bed19ac6f599ff93
YoanEnchev <joanbojchev@gmail.com>
2017-12-27 18:54:45 +0200

    Added another MySQL and PHP app.

--- 13_MySQLAndPHPAdvanced/db_config.php
+++ 13_MySQLAndPHPAdvanced/db_config.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_host = "localhost";
-$db_name = "cars";
-$db_user = "root";
-$db_password = '';
+$db_host = "<your host>";
+$db_name = "<your DB name>";
+$db_user = "<your username>";
+$db_password = '<your password>';
 $db = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password,
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
